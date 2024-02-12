aeea0ee07d894ecb7ca04ae4f4e78e6ed577d6c6
Ramiro Platero <ramiplatero11@gmail.com>
2024-02-12 12:18:29 -0300

    feat: add route to add product to cart

--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -2,26 +2,40 @@ import { Router } from "express";
 import CartManager from "../../CartManager.js";
 
 const route = Router();
-const cartManager = new CartManager()
+const cartManager = new CartManager();
 
 route.post("/", async (_req, res, next) => {
   try {
-    await cartManager.createCart()
+    await cartManager.createCart();
 
-    return res.status(200).json({message: "Created cart successfully."})
+    return res.status(200).json({ message: "Created cart successfully." });
   } catch (error) {
     return next(error);
   }
 });
 
-route.get("/", async (_req,res,next) => {
+route.get("/", async (_req, res, next) => {
   try {
-    const carts = await cartManager.readCart()
+    const carts = await cartManager.readCart();
 
-    return res.status(200).json(carts)
+    return res.status(200).json(carts);
   } catch (error) {
-    return next(error)
+    return next(error);
   }
-})
+});
+
+route.post("/:cid/product/:pid", async (req, res, next) => {
+  try {
+    const { cid, pid } = req.params;
+
+    await cartManager.addProductToCart({ cid, pid });
+
+    return res
+      .status(200)
+      .json({ message: "Product added to cart successfully!" });
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default route;
