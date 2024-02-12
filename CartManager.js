5474dd2880d593ec1ce52e5c30a38b6c744dc790
Ramiro Platero <ramiplatero11@gmail.com>
2024-02-12 11:40:08 -0300

    feat: add CartManager class

---
+++ CartManager.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from "uuid";
+import { AppError } from "./src/helpers/AppError";
+import fs from 'fs'
+
+class CartManager {
+  constructor() {
+    this.path = "./carts.json";
+  }
+
+  readCart = async () => {
+        if (fs.existsSync(this.path)) {
+            const data = await fs.promises.readFile(this.path, "utf-8");
+            const cart = JSON.parse(data);
+            return cart;
+          } else {
+            return [];
+          }
+  };
+
+  createCart = async () => {
+    const newCart = {
+      id: uuidv4(),
+      products: [],
+    };
+
+    const cart = await this.readCart()
+    cart.push(newCart)
+
+    const json = JSON.stringify(cart, null, 2);
+    await fs.promises.writeFile(this.path,json)
+  };
+
+
+}
+
+export default CartManager
