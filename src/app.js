b942fb363c90db21d185d9d3cc37fc5cf493b471
Ramiro Platero <ramiplatero11@gmail.com>
2024-02-21 13:36:07 -0300

    setup socket.io and handlebars

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
 import express from "express";
 import morgan from "morgan";
 import productsRoute from "./routes/products.route.js";
-import cartsRoute from './routes/carts.route.js'
+import cartsRoute from "./routes/carts.route.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import viewsRouter from "./routes/views.router.js";
+import __dirname from "./utils.js";
+import { Server } from "socket.io";
+import handlebars from 'express-handlebars'
 
 const app = express();
 
 app.set("PORT", 8080);
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(express.json())
 
-app.use("/api/products", productsRoute)
-app.use("/api/carts", cartsRoute)
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
 
-app.use(errorHandler)
+app.use(express.static(`${__dirname}/public`));
 
-app.listen(app.get("PORT"), () =>
+app.use("/", viewsRouter);
+app.use("/api/products", productsRoute);
+app.use("/api/carts", cartsRoute);
+
+app.use(errorHandler);
+
+const httpServer = app.listen(app.get("PORT"), () =>
   console.log("Server running on port", app.get("PORT"))
 );
+const io = new Server(httpServer);
+app.set("io", io);
+
+io.on("connection", (socket) => {
+  console.log("connected", socket.id);
+})
+
+export default app
