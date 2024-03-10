c3290e66ad62ff052930b4263aa31090edf33a32
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-10 18:53:33 -0300

    feat: add products schemas for each route

---
+++ src/schemas/product.schema.js
@@ -0,0 +1,68 @@
+import z from "zod";
+import mongoose from "mongoose";
+
+const product = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title must be a string",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+    invalid_type_error: "Description must be a string",
+  }),
+  code: z.string({
+    required_error: "Code is required",
+    invalid_type_error: "Code must be a string",
+  }),
+  stock: z.number({
+    required_error: "Stock is required",
+    invalid_type_error: "Stock must be a number",
+  }),
+  price: z.number({
+    required_error: "Price is required",
+    invalid_type_error: "Price must be a number",
+  }),
+  category: z.string({
+    required_error: "Category is required",
+    invalid_type_error: "Category must be a string",
+  }),
+  status: z
+    .boolean({
+      invalid_type_error: "Status must be a boolean",
+    })
+    .optional()
+    .default(true),
+  thumbnails: z
+    .array(
+      z.string({
+        invalid_type_error: "Thumbnails must be an array of strings",
+      })
+    )
+    .optional(),
+});
+
+export const createBodyProductSchema = product.extend({
+  thumbnails: z
+    .array(
+      z.string({
+        invalid_type_error: "Thumbnails must be an array of strings",
+      })
+    )
+    .optional()
+    .default([]),
+});
+export const updateBodyProductSchema = product
+  .extend({
+    status: z.boolean(),
+  })
+  .partial()
+  .strict();
+export const productIDSchema = z.object({
+  pid: z
+    .string({
+      required_error: "ID is required.",
+    })
+    .refine((value) => {
+      return mongoose.Types.ObjectId.isValid(value);
+    }, "You entered an invalid ID type."),
+});
