f4b179f32a073ca57318045466131ad8586a6ba6
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-10 18:55:20 -0300

    feat: add product mongoose model

---
+++ src/dao/database/models/product.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  thumbnails: [{type: String}]
+});
+
+export default model("Product", schema);
