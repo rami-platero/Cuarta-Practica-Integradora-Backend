5ec63885f7163248e8fef45ef793f1ef9586581e
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-10 19:07:25 -0300

    refactor: restructure routes to use controllers and validation middlewares

--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -1,41 +1,11 @@
 import { Router } from "express";
-import CartManager from "../../CartManager.js";
+import { addProductToCart, createCart, getAllCarts } from "../controllers/carts.controller.js";
+import { validateAddProductToCart } from "../middlewares/validate.js";
 
 const route = Router();
-const cartManager = new CartManager();
 
-route.post("/", async (_req, res, next) => {
-  try {
-    await cartManager.createCart();
-
-    return res.status(200).json({ message: "Created cart successfully." });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-route.get("/", async (_req, res, next) => {
-  try {
-    const carts = await cartManager.readCart();
-
-    return res.status(200).json(carts);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-route.post("/:cid/product/:pid", async (req, res, next) => {
-  try {
-    const { cid, pid } = req.params;
-
-    await cartManager.addProductToCart({ cid, pid });
-
-    return res
-      .status(200)
-      .json({ message: "Product added to cart successfully!" });
-  } catch (error) {
-    return next(error);
-  }
-});
+route.post("/", createCart);
+route.get("/", getAllCarts);
+route.post("/:cid/product/:pid", validateAddProductToCart, addProductToCart);
 
 export default route;
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,91 +1,13 @@
 import { Router } from "express";
-import ProductManager from "../../ProductManager.js";
-import { AppError } from "../helpers/AppError.js";
-import app from '../app.js'
+import { validateCreateProduct, validateDeleteProduct, validateGetProductById, validateUpdateProduct } from "../middlewares/validate.js";
+import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "../controllers/product.controller.js";
 
 const route = Router();
-const productManager = new ProductManager();
 
-route.get("/", async (req, res, next) => {
-  try {
-    const { limit } = req.query;
-
-    if (limit && (isNaN(limit) || Number(limit) < 0)) {
-        throw new AppError(400,{ message: "Invalid limit query." });
-    }
-
-    const products = await productManager.readProducts();
-
-    if (!limit) {
-      return res.status(200).json({ products });
-    }
-
-    const limitedProducts = products.slice(0, Number(limit));
-    return res.status(200).json({ products: limitedProducts });
-  } catch (error) {
-    return next();
-  }
-});
-
-route.get("/:pid", async (req, res, next) => {
-  try {
-    const { pid } = req.params;
-
-    if (!pid || isNaN(pid)) {
-      throw new AppError(400,{ message: "Invalid or missing ID." });
-    }
-
-    const foundProduct = await productManager.getProductById(pid);
-
-    if (!foundProduct) {
-      throw new AppError(404,{ message: "Product not found" });
-    }
-
-    return res.status(200).json({ product: foundProduct });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-route.post("/", async (req, res, next) => {
-  try {
-    const io = app.get("io")
-    const body = req.body;
-
-    const newProduct = await productManager.addProduct(body);
-
-    io.emit("add_product", newProduct)
-
-    return res.status(200).json({ message: "Product added successfully." });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-route.put("/:pid", async (req, res, next) => {
-  try {
-    const body = req.body;
-    const { pid } = req.params;
-
-    await productManager.updateProduct(pid, body);
-
-    return res.status(200).json({ message: "Product updated successfully." });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-route.delete("/:pid", async (req, res, next) => {
-  try {
-    const io = app.get("io")
-    const {pid} = req.params
-
-    await productManager.deleteProduct(pid)
-    io.emit("delete_product", pid)
-    return res.sendStatus(204)
-  } catch (error) {
-    return next(error);
-  }
-});
+route.get("/", getAllProducts);
+route.get("/:pid", validateGetProductById, getProductById);
+route.post("/", validateCreateProduct, createProduct);
+route.put("/:pid", validateUpdateProduct, updateProduct);
+route.delete("/:pid", validateDeleteProduct, deleteProduct);
 
 export default route;
