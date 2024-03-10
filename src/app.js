0a2a0da6a0e57c0ebf6510f8f838f88a29b3a7e0
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-10 18:52:56 -0300

    feat: add middleware for /api/messages route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import productsRoute from "./routes/products.route.js";
 import cartsRoute from "./routes/carts.route.js";
+import messagesRoute from './routes/messages.route.js'
 import { errorHandler } from "./middlewares/errorHandler.js";
 import viewsRouter from "./routes/views.router.js";
 import __dirname from "./utils.js";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
-import mongoose from "mongoose";
 import { connectDB } from "./dao/database/db.js";
 
 const app = express();
@@ -27,6 +27,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/", viewsRouter);
 app.use("/api/products", productsRoute);
 app.use("/api/carts", cartsRoute);
+app.use("/api/messages", messagesRoute)
 
 app.use(errorHandler);
 
