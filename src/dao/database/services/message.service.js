52dfea4b5a550f4bb40fbd8a67f7832978aa5683
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-10 19:02:32 -0300

    feat: create MessageService to handle db logic (create, getAllMessages)

---
+++ src/dao/database/services/message.service.js
@@ -0,0 +1,13 @@
+import Message from "../models/message.model.js";
+
+class MessageService {
+  static getAllMessages = async () => {
+    return await Message.find();
+  };
+
+  static createMessage = async (body) => {
+    return await Message.create({ message: body.message, user: body.user });
+  };
+}
+
+export default MessageService;
