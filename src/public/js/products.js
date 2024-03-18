906bf95a38abc7d50e7785710d29ed1be40041ce
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-18 15:32:33 -0300

    feat: add js script file to handle add item to cart logic

---
+++ src/public/js/products.js
@@ -0,0 +1,60 @@
+const notifyError = (message) => {
+  Toastify({
+    text: `ERROR: ${message}`,
+    position: "left",
+    gravity: "bottom",
+    style: {
+      background: "#ff383f",
+    },
+  }).showToast();
+};
+
+const notifySuccess = (message) => {
+  Toastify({
+    text: message,
+    position: "left",
+    gravity: "bottom",
+    style: {
+      background: "#0eb039",
+    },
+  }).showToast();
+};
+
+const formatValidationErrors = (errors) => {
+  let formattedErrors = [];
+
+  errors.forEach((error) => {
+    formattedErrors.push(error.message);
+  });
+
+  return formattedErrors;
+};
+
+const addProductToCart = async (id) => {
+  try {
+    await axios.post(`/api/carts/65ee23e3ed136b6cc4bebfb5/products/${id}`);
+    return notifySuccess("Product added to cart successfully.");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response.status === 409) {
+        const formattedErrors = formatValidationErrors(
+          error.response.data.error
+        );
+        return formattedErrors.forEach((errorMessage) => {
+          notifyError(errorMessage);
+        });
+      }
+
+      if ("message" in error.response.data) {
+        return notifyError(error.response.data.message);
+      }
+    }
+    return notifyError("Internal server error.");
+  }
+};
+
+document.addEventListener("click", async (e) => {
+  if (e.target.matches(".addToCart")) {
+    await addProductToCart(e.target.dataset.id);
+  }
+});
