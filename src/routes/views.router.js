41e79b685ad7c427903f92e341509ad0a24bd3e5
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-18 15:32:10 -0300

    feat: add new views routes (cart single page and products with pagination implementation)

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import MessageService from "../dao/database/services/message.service.js";
 import ProductService from "../dao/database/services/product.service.js";
+import { validateGetCartById, validateGetProducts } from "../middlewares/validate.js";
+import CartService from "../dao/database/services/carts.service.js";
 
-const route = Router();
+const router = Router();
 
-route.get("/", async (_req, res) => {
+router.get("/", async (_req, res) => {
   const products = await ProductService.getAllProducts();
   return res.render("home", {
     products: products.map((p) => {
@@ -13,7 +15,7 @@ route.get("/", async (_req, res) => {
   });
 });
 
-route.get("/realTimeProducts", async (_req, res) => {
+router.get("/realTimeProducts", async (_req, res) => {
   const products = await ProductService.getAllProducts();
   return res.render("realTimeProducts", {
     products: products.map((p) => {
@@ -22,13 +24,33 @@ route.get("/realTimeProducts", async (_req, res) => {
   });
 });
 
-route.get("/chat", async (_req, res) => {
+router.get("/chat", async (_req, res) => {
   const messages = await MessageService.getAllMessages();
   return res.render("chat", {
-    messages: messages.map((m) => {
-      return m.toJSON();
-    }),
+    messages,
   });
 });
 
-export default route;
+router.get("/products", validateGetProducts, async (req, res) => {
+  const queries = req.query;
+  const queryString = req._parsedOriginalUrl.query;
+
+  const result = await ProductService.getProducts(queries);
+  return res.render("products", {
+    products: result.docs,
+    totalPages: result.totalPages,
+    page: result.page,
+    queries: queryString,
+  });
+});
+
+router.get("/carts/:cid", validateGetCartById, async (req, res) => {
+  const {cid} = req.params
+
+  const foundCart = await CartService.getCartByID(cid)
+  return res.render("cart", {
+    cart: foundCart
+  })
+});
+
+export default router;
