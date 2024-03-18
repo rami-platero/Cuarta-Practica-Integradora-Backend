1f439052c3b4c2f8c04fd0e607a25e8466c62cc8
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-18 14:47:09 -0300

    feat: add custom handlebars helpers (times, ifEquals, if Greater, updateQueryParam)

---
+++ src/helpers/handlebars.helpers.js
@@ -0,0 +1,37 @@
+const hbsHelpers = {
+  times: function (n, block) {
+    {
+      let result = "";
+      for (let i = 0; i < n; i++) {
+        result += block.fn(i + 1);
+      }
+      return result;
+    }
+  },
+  ifEquals: function (a, b, options) {
+    if (a === b) {
+      return options.fn(this);
+    }
+
+    return options.inverse(this);
+  },
+  ifGreater: function (a,b,options) {
+    if (a > b) {
+        return options.fn(this);
+      }
+  
+    return options.inverse(this);
+  },
+  updateQueryParam: function (param, value, queries) {
+    try {
+      const url = "http://localhost:8080/products";
+      const urlParams = new URLSearchParams(queries);
+      urlParams.set(param, value);
+      return `${url}?${urlParams.toString()}`;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export default hbsHelpers;
