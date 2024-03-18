f8a08c46ed14a04f1448d1e279d697a0110114d3
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-18 15:20:44 -0300

    refactor: separate cartID zod component. feat: add schemas for updateProductQuantity and updateCartProductsArray

--- src/schemas/carts.schema.js
+++ src/schemas/carts.schema.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
 import { z } from "zod";
 
-export const addProductToCartSchema = z.object({
+const cartID = z
+  .string({
+    required_error: "Cart ID is required.",
+  })
+  .refine((value) => {
+    return mongoose.Types.ObjectId.isValid(value);
+  }, "You entered an invalid cart ID type.");
+
+export const cartProductsSchema = z.object({
   pid: z
     .string({
       required_error: "Product ID is required.",
@@ -9,11 +17,31 @@ export const addProductToCartSchema = z.object({
     .refine((value) => {
       return mongoose.Types.ObjectId.isValid(value);
     }, "You entered an invalid product ID type."),
-  cid: z
-    .string({
-      required_error: "Cart ID is required.",
-    })
-    .refine((value) => {
-      return mongoose.Types.ObjectId.isValid(value);
-    }, "You entered an invalid cart ID type."),
+  cid: cartID,
 });
+
+export const cartIDSchema = z.object({
+  cid: cartID,
+});
+
+export const updateProductQuantitySchema = z.object({
+  quantity: z.number({
+    required_error: "You must enter a quantity to update a product.",
+  }),
+});
+
+export const validateCartProductsArraySchema = z.array(
+  z.object({
+    quantity: z.number({
+      required_error: "Quantity is required for every cart product.",
+      invalid_type_error: "Quantity must be a number.",
+    }),
+    product: z
+      .string({
+        required_error: "Product ID is required.",
+      })
+      .refine((value) => {
+        return mongoose.Types.ObjectId.isValid(value);
+      }, "You entered an invalid product ID type."),
+  })
+);
