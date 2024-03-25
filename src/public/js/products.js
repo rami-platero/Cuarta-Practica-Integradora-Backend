f08742d13de81f48aa7531e60be4d76331366eed
rami-platero <ramiplatero11@gmail.com>
2024-03-25 19:22:42 -0300

    feat: add handleLogout function. refactor: create a function to reuse for fetch error handling

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -30,31 +30,47 @@ const formatValidationErrors = (errors) => {
   return formattedErrors;
 };
 
+const handleErrors = (error) => {
+  if (axios.isAxiosError(error)) {
+    if (error.response.status === 409) {
+      const formattedErrors = formatValidationErrors(error.response.data.error);
+      return formattedErrors.forEach((errorMessage) => {
+        notifyError(errorMessage);
+      });
+    }
+
+    if ("message" in error.response.data) {
+      return notifyError(error.response.data.message);
+    }
+  }
+  return notifyError("Internal server error.");
+};
+
 const addProductToCart = async (id) => {
   try {
     await axios.post(`/api/carts/65ee23e3ed136b6cc4bebfb5/products/${id}`);
     return notifySuccess("Product added to cart successfully.");
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (error.response.status === 409) {
-        const formattedErrors = formatValidationErrors(
-          error.response.data.error
-        );
-        return formattedErrors.forEach((errorMessage) => {
-          notifyError(errorMessage);
-        });
-      }
-
-      if ("message" in error.response.data) {
-        return notifyError(error.response.data.message);
-      }
-    }
-    return notifyError("Internal server error.");
+    return handleErrors(error)
   }
 };
+
+const handleLogout = async () => {
+  try {
+    const result = await axios.post("/api/auth/logout")
+    if(result.status === 200){
+      window.location.replace("/login")
+    }
+  } catch (error) {
+    return handleErrors(error)
+  }
+}
 
 document.addEventListener("click", async (e) => {
   if (e.target.matches(".addToCart")) {
     await addProductToCart(e.target.dataset.id);
+  }
+  if(e.target.matches("#logout")){
+    await handleLogout()
   }
 });
