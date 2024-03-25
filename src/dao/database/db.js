5feff0878b57d6e31957e6a254f2196515b70d57
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-24 21:32:44 -0300

    refactor: update MongoDB connection to use config.MONGODB_URI

--- src/dao/database/db.js
+++ src/dao/database/db.js
@@ -1,12 +1,12 @@
-import 'dotenv/config'
 import { connect } from "mongoose";
+import { config } from "../../config.js";
 
-export const connectDB = async () =>{
-    try {
-        await connect(process.env.MONGODB_URI)
-        console.log('connected to db')
-
-    } catch (error) {
-        console.log("couldn't connect to server")
-    }
-}
+export const connectDB = async () => {
+  try {
+    // @ts-ignore
+    await connect(config.MONGODB_URI);
+    console.log("connected to db");
+  } catch (error) {
+    console.log("couldn't connect to server");
+  }
+};
