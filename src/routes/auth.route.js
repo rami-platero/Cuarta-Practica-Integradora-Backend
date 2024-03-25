321f5b948a79833c6ecc04262a792d9bebe4d7f7
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-24 21:33:51 -0300

    feat: add auth route file

---
+++ src/routes/auth.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.get("/", )
+router.post("/register", )
+router.post("/login", )
+router.post("/logout")
+
+export default router
