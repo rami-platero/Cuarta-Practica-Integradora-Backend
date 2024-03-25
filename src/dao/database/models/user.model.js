14e93c01f9b27a963441ec632265215ced9527c3
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-24 21:22:23 -0300

    chore: add mongoose User model

---
+++ src/dao/database/models/user.model.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = model("users", schema)
+export default User
