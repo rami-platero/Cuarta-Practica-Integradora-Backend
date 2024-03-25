f21d5394507cdb646ac29739881cb503366402a3
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-24 21:31:08 -0300

    chore: add MONGODB URI to config.js to reuse

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
+import 'dotenv/config'
+
 export const config = {
   productsPath: "./src/dao/FileSystem/files/products.json",
   cartsPath: "./src/dao/FileSystem/files/carts.json",
+  MONGODB_URI: process.env.MONGODB_URI
 };
