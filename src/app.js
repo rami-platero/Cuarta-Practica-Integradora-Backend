b983ba161dec77214e5fae7f2884d3ba65c0bc76
Ramiro Platero <ramiplatero11@gmail.com>
2024-03-24 21:33:08 -0300

    chore: setup sessions and MongoStore

--- src/app.js
+++ src/app.js
@@ -5,12 +5,17 @@ import productsRoute from "./routes/products.route.js";
 import cartsRoute from "./routes/carts.route.js";
 import messagesRoute from "./routes/messages.route.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import viewsRouter from "./routes/views.router.js";
+import viewsRoute from "./routes/views.route.js";
+import authRoute from './routes/auth.route.js'
 import __dirname from "./utils.js";
 import { swaggerDocs } from "./utils/swagger.js";
 import handlebars from "express-handlebars";
 import { connectDB } from "./dao/database/db.js";
 import hbsHelpers from "./helpers/handlebars.helpers.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { config } from "./config.js";
 
 const app = express();
 
@@ -18,6 +23,24 @@ app.set("PORT", 8080);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: config.MONGODB_URI,
+      mongoOptions: {
+        // @ts-ignore
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      ttl: 15,
+    }),
+    secret: "adadasda",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(morgan("dev"));
 
 const hbs = handlebars.create({
@@ -31,10 +54,11 @@ app.set("view engine", "handlebars");
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use("/", viewsRouter);
+app.use("/", viewsRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/carts", cartsRoute);
 app.use("/api/messages", messagesRoute);
+app.use("/api/auth/", authRoute);
 
 app.use(errorHandler);
 
