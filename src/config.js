7701482cd67e7450ed56d2162ed8c572145dcac5
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-01 19:41:53 -0300

    chore: add github oauth keys (client, secret keys) to config file

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ import 'dotenv/config'
 export const config = {
   productsPath: "./src/dao/FileSystem/files/products.json",
   cartsPath: "./src/dao/FileSystem/files/carts.json",
-  MONGODB_URI: process.env.MONGODB_URI
+  MONGODB_URI: process.env.MONGODB_URI,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET
 };
