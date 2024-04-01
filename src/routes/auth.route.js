c22174835d74e4575ef789fddbefbe80fd2ab93e
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-01 19:45:18 -0300

    chore: replace old routes with passport routes and oauth github callback routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,52 @@
 import { Router } from "express";
-import { Login, Logout, Register } from "../controllers/user.controller.js";
+import { Logout } from "../controllers/user.controller.js";
+import passport from "passport";
 
-const router = Router()
+const router = Router();
 
-router.post("/register", Register)
-router.post("/login", Login)
-router.post("/logout", Logout)
+router.post("/logout", Logout);
 
-export default router
+router.post(
+  "/register",
+  passport.authenticate("register", { failureRedirect: "/failregister" }),
+  async (_req, res) => {
+    return res.send({ status: "sucess", message: "user registered" });
+  }
+);
+router.get("/failregister", async (_req, res) => {
+  return res.send({ error: "Failed" });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("login", { failureRedirect: "/faillogin" }),
+  async (req, res) => {
+    req.session.user = req.user;
+    return res.status(200).send({ status: "success", user: req.user });
+  }
+);
+
+router.get("/faillogin", (_req, res) => {
+  return res.send({ error: "failed login" });
+});
+
+router.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+
+router.get(
+  "/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  async (req, res, next) => {
+    try {
+      // @ts-ignore
+      req.session.user = req.user;
+      return res.redirect("/products");
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+export default router;
