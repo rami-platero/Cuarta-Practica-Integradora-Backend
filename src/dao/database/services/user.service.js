715e0449220a2875a4e63734e47d0d15bbfd8610
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-01 19:48:46 -0300

    feat: add new param property (strategy) in register method to handle user registering with github and add a new method findGitHubEmail to handle fetch of github emails when registering

--- src/dao/database/services/user.service.js
+++ src/dao/database/services/user.service.js
@@ -3,8 +3,17 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
 class UserService {
-  static register = async ({ username, email, password: pass }) => {
+  static register = async ({ username, email, password: pass, strategy }) => {
     try {
+      // registering with GitHub
+      if (strategy === "github") {
+        return await User.create({
+          email,
+          password: null,
+          username,
+        }).toObject();
+      }
+
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(pass, salt);
 
@@ -42,6 +51,21 @@ class UserService {
 
     const { password, ...loggedUser } = foundUser.toObject();
     return loggedUser;
+  };
+
+  static findGitHubEmail = async (accessToken) => {
+    const res = await fetch("https://api.github.com/user/emails", {
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    });
+    const emails = await res.json();
+    if (!emails || emails.length === 0) {
+      throw new AppError(500, { message: "Internal server error." });
+    }
+    // Sort by primary email - the user may have several emails, but only one of them will be primary
+    const sortedEmails = emails.sort((a, b) => b.primary - a.primary);
+    return sortedEmails[0].email;
   };
 }
 
