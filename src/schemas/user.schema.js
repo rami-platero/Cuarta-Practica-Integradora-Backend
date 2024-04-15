c37df45a6b1ba1c9c3b4cb71ee5ddd1aa841fab1
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-15 12:05:37 -0300

    feat: add register user zod schema

---
+++ src/schemas/user.schema.js
@@ -0,0 +1,22 @@
+import z from 'zod'
+
+export const registerSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    age: z
+    .any()
+    .refine(
+      (value) => {
+        return !isNaN(value);
+      },
+      {
+        message: "Age must be a number.",
+      }
+    ),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirPassword"],
+});
