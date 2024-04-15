380bebaf0878e1ec8add73f28ee1df5668487130
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-15 11:19:32 -0300

    feat: add JWT_SECRET_KEY to variable config file

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ export const config = {
   cartsPath: "./src/dao/FileSystem/files/carts.json",
   MONGODB_URI: process.env.MONGODB_URI,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
 };
