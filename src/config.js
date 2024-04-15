b255f37184fb658df9b980d89bc0413ddc502d4c
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-15 12:02:49 -0300

    feat: add SESSION_SECRET_CODE variable to config file

--- src/config.js
+++ src/config.js
@@ -6,5 +6,6 @@ export const config = {
   MONGODB_URI: process.env.MONGODB_URI,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  SESSION_SECRET_CODE: process.env.SESSION_SECRET_CODE
 };
