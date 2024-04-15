01e45510029d64466aa6ccb902adb194430ad853
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-15 12:13:06 -0300

    refactor: replace main auth routes that used Local strategy with ones that use jwt strategy

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,34 +1,13 @@
 import { Router } from "express";
-import { Logout } from "../controllers/user.controller.js";
+import { Login, Logout, Register, loginWithGitHub } from "../controllers/user.controller.js";
 import passport from "passport";
+import { validateRegisterUser } from "../middlewares/validate.js";
 
 const router = Router();
 
 router.post("/logout", Logout);
-
-router.post(
-  "/register",
-  passport.authenticate("register", { failureRedirect: "/failregister" }),
-  async (_req, res) => {
-    return res.send({ status: "sucess", message: "user registered" });
-  }
-);
-router.get("/failregister", async (_req, res) => {
-  return res.send({ error: "Failed" });
-});
-
-router.post(
-  "/login",
-  passport.authenticate("login", { failureRedirect: "/faillogin" }),
-  async (req, res) => {
-    req.session.user = req.user;
-    return res.status(200).send({ status: "success", user: req.user });
-  }
-);
-
-router.get("/faillogin", (_req, res) => {
-  return res.send({ error: "failed login" });
-});
+router.post("/register", validateRegisterUser, Register)
+router.post("/login", Login)
 
 router.get(
   "/github",
@@ -38,15 +17,7 @@ router.get(
 router.get(
   "/github/callback",
   passport.authenticate("github", { failureRedirect: "/login" }),
-  async (req, res, next) => {
-    try {
-      // @ts-ignore
-      req.session.user = req.user;
-      return res.redirect("/products");
-    } catch (error) {
-      return next(error);
-    }
-  }
+  loginWithGitHub
 );
 
 export default router;
