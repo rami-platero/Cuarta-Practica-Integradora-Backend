e0d57af1eaf96192bfe75f23ad92bc3cea7ee113
Ramiro Platero <ramiplatero11@gmail.com>
2024-04-15 12:21:40 -0300

    feat: add loginWithGithHub function to handle jwt token when authenticating with github, and handle jwt + cookie management in the rest of the functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,31 @@
 import UserService from "../dao/database/services/user.service.js";
+import { generateJWToken } from "../utils/jwt.js";
 
 export const Register = async (req, res, next) => {
   try {
-    const { email, username, password } = req.body;
+    const { lastName, firstName, age, email, password } = req.body;
 
     const newUser = await UserService.register({
       email,
-      username,
+      firstName,
+      lastName,
+      age,
       password,
     });
 
-    req.session.user = newUser;
+    const tokenUser = {
+      name: user.username,
+      age: newUser.age,
+      email: newUser.email,
+      role: newUser.role
+    }
+
+    const accessToken = generateJWToken(tokenUser);
+
+    res.cookie("jwtCookieToken", accessToken, {
+      maxAge: 120000,
+      httpOnly: true,
+    });
 
     return res.status(201).json({ status: "success", user: newUser });
   } catch (error) {
@@ -22,20 +37,56 @@ export const Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    const loggedUser = await UserService.login({ email, password });
+    const user = await UserService.login({ email, password });
 
-    req.session.user = loggedUser;
-    return res.status(200).json({ status: "success", user: loggedUser });
+    const tokenUser = {
+      name: user.username,
+      age: user.age,
+      email: user.email,
+      role: user.role
+    }
+
+    const accessToken = generateJWToken(tokenUser);
+
+    res.cookie("jwtCookieToken", accessToken, {
+      maxAge: 120000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json({ status: "success", user });
   } catch (error) {
     return next(error);
   }
 };
 
-export const Logout = async (req, res, next) => {
+export const Logout = async (_req, res, next) => {
   try {
-    req.session.destroy();
+    res.clearCookie("jwtCookieToken")
     return res.status(200).json({ message: "Logged out successfully!" });
   } catch (error) {
     return next(error);
   }
 };
+
+export const loginWithGitHub = async (req,res,next) => {
+  try {
+
+    const tokenUser = {
+      name: req.user.username,
+      age: req.user.age,
+      email: req.user.email,
+      role: req.user.role
+    }
+
+    const accessToken = generateJWToken(tokenUser);
+
+    res.cookie("jwtCookieToken", accessToken, {
+      maxAge: 120000,
+      httpOnly: true,
+    });
+
+    return res.redirect("/products");
+  } catch (error) {
+    return next(error)
+  }
+}
