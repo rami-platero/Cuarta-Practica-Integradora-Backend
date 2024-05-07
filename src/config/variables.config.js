70cc411a37ce272b0ae076d967515dfe77428aed
Fabrizio Platero <fabriplatero88@gmail.com>
2024-05-06 22:54:55 -0300

    feat: add persistence variable from commander config

--- src/config/variables.config.js
+++ src/config/variables.config.js
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import program from './commander.config.js';
 
 export const config = {
   productsPath: "./src/dao/FileSystem/files/products.json",
@@ -9,5 +10,6 @@ export const config = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   SESSION_SECRET_CODE: process.env.SESSION_SECRET_CODE,
   ADMIN_EMAIL: process.env.ADMIN_EMAIL,
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  persistence: program.opts().persist
 };
