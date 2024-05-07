9efb55a0569f3c07d46e1e76e56893ea02e838e2
rami-platero <ramiplatero11@gmail.com>
2024-05-07 00:31:35 -0300

    feat: check if user is authenticated before sending message

--- src/routes/messages.route.js
+++ src/routes/messages.route.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { createMessage, getMessages } from "../controllers/messages.controller.js";
+import {
+  createMessage,
+  getMessages,
+} from "../controllers/messages.controller.js";
 import { validateCreateMessage } from "../middlewares/validate.js";
+import { isAuthenticated } from "../middlewares/authJwt.js";
+import { passportCall } from "../middlewares/passport.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/", getMessages)
-router.post("/", validateCreateMessage, createMessage)
+router.get("/", getMessages);
+router.post(
+  "/",
+  validateCreateMessage,
+  passportCall("jwt"),
+  isAuthenticated,
+  createMessage
+);
 
-export default router
+export default router;
