9c167d09989a519977dcaa2c7878c2ea6d6d00bb
rami-platero <ramiplatero11@gmail.com>
2024-05-07 00:32:41 -0300

    refactor: switch 'json' method is res for 'send' method

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,8 +2,7 @@ import { AppError } from "../helpers/AppError.js";
 
 export const errorHandler = (error, _req, res, _next) => {
   if (error instanceof AppError) {
-    console.log(error);
-    return res.status(error.statusCode).json(JSON.parse(error.message));
+    return res.status(error.statusCode).send(JSON.parse(error.message));
   }
   return res.status(500).json({ error });
 };
