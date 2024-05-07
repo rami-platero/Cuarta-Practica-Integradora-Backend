4072523da0a02f467c2e060de8c8d120320ec049
rami-platero <ramiplatero11@gmail.com>
2024-05-07 00:31:11 -0300

    feat: check if user is admin for create/update/delete product operations

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { validateCreateProduct, validateDeleteProduct, validateGetProductById, validateGetProducts, validateUpdateProduct } from "../middlewares/validate.js";
 import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from "../controllers/product.controller.js";
+import { isAuthorized } from "../middlewares/authJwt.js";
+import { passportCall } from "../middlewares/passport.js";
 
 const router = Router();
 
 router.get("/", validateGetProducts, getProducts);
 router.get("/:pid", validateGetProductById, getProductById);
-router.post("/", validateCreateProduct, createProduct);
-router.put("/:pid", validateUpdateProduct, updateProduct);
-router.delete("/:pid", validateDeleteProduct, deleteProduct);
+router.post("/", validateCreateProduct, passportCall("jwt"), isAuthorized("admin"), createProduct);
+router.put("/:pid", validateUpdateProduct, passportCall("jwt"), isAuthorized("admin"),updateProduct);
+router.delete("/:pid", validateDeleteProduct, passportCall("jwt"), isAuthorized("admin"),deleteProduct);
 
 export default router;
