2f89125169d4743cc1f09cbe1686ab4a3c38a836
rami-platero <ramiplatero11@gmail.com>
2024-05-07 01:44:40 -0300

    feat: create ticketService instance and pass it to CartsRepository constructor

--- src/services/service.js
+++ src/services/service.js
@@ -1,10 +1,12 @@
-import { cartsDao, messageDao, productDao, userDao } from "./factory.js";
+import { cartsDao, messageDao, productDao, ticketDao, userDao } from "./factory.js";
 import CartsRepository from "./repositories/carts.repository.js";
 import MessageRepository from "./repositories/message.repository.js";
 import ProductRepository from "./repositories/product.repository.js";
+import TicketRepository from "./repositories/ticket.repository.js";
 import UserRepository from "./repositories/user.repository.js";
 
 export const userService = new UserRepository(userDao)
 export const productService = new ProductRepository(productDao)
-export const cartsService = new CartsRepository(cartsDao, productService)
+export const ticketService = new TicketRepository(ticketDao)
+export const cartsService = new CartsRepository(cartsDao, productService, ticketService)
 export const messageService = new MessageRepository(messageDao)
