3006096f708d4f28e03d0672c53ca6e0521f4ecb
rami-platero <ramiplatero11@gmail.com>
2024-05-07 01:43:55 -0300

    feat: create ticketDao in factory file and fix imports

--- src/services/factory.js
+++ src/services/factory.js
@@ -1,30 +1,33 @@
 import { config } from "../config/variables.config.js";
 import { initMongoConnection } from "./dao/mongo/db.js"
 
-export let productDao, cartsDao, userDao, messageDao;
+export let productDao, cartsDao, userDao, messageDao, ticketDao;
 
 switch (config.persistence) {
   case "mongodb":
     initMongoConnection();
     const { default: ProductMongo } = await import(
-      "../services/dao/mongo/classes/product.dao.js"
+      "./dao/mongo/classes/product.dao.js"
     );
     productDao = new ProductMongo;
 
     const { default: MessageMongo } = await import(
-      "../services/dao/mongo/classes/message.dao.js"
+      "./dao/mongo/classes/message.dao.js"
     );
     messageDao = new MessageMongo;
 
     const { default: CartsMongo } = await import(
-      "../services/dao/mongo/classes/carts.dao.js"
+      "./dao/mongo/classes/carts.dao.js"
     );
     cartsDao = new CartsMongo;
 
     const { default: UserMongo } = await import(
-      "../services/dao/mongo/classes/user.dao.js"
+      "./dao/mongo/classes/user.dao.js"
     );
     userDao = new UserMongo;
+
+    const {default: TicketMongo} = await import('./dao/mongo/classes/ticket.dao.js')
+    ticketDao = new TicketMongo
     break;
 
   default:
