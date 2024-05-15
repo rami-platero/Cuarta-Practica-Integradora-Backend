d093ad506d3ab83ac4f5c07081a1bf52da611d18
rami-platero <ramiplatero11@gmail.com>
2024-05-15 16:56:22 -0300

    feat: add mockingproducts endpoint to get fake products

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { validateCreateProduct, validateDeleteProduct, validateGetProductById, validateGetProducts, validateUpdateProduct } from "../middlewares/validate.js";
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getMockingProducts, getProductById, getProducts, updateProduct } from "../controllers/product.controller.js";
 import { isAuthorized } from "../middlewares/authJwt.js";
 import { passportCall } from "../middlewares/passport.js";
 
 const router = Router();
 
 router.get("/", validateGetProducts, getProducts);
+router.get("/mockingproducts", getMockingProducts)
 router.get("/:pid", validateGetProductById, getProductById);
 router.post("/", validateCreateProduct, passportCall("jwt"), isAuthorized("admin"), createProduct);
 router.put("/:pid", validateUpdateProduct, passportCall("jwt"), isAuthorized("admin"),updateProduct);
