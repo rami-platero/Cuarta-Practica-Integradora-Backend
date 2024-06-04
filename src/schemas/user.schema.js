cfbea91aec0abcf37cf3f1e7e1c96be51342b40c
rami-platero <ramiplatero11@gmail.com>
2024-06-03 23:56:14 -0300

    feat: add reset-password body schema and fix typo in registerSchema

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -18,5 +18,13 @@ export const registerSchema = z.object({
     confirmPassword: z.string(),
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
-    path: ["confirPassword"],
+    path: ["confirmPassword"],
+});
+
+export const resetPasswordSchema = z.object({
+  password: z.string().min(8),
+  confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
 });
