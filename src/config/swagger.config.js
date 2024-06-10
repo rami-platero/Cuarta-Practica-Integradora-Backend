28d93233d926326e933b7fc3473cae250793d52c
rami-platero <ramiplatero11@gmail.com>
2024-06-10 15:34:59 -0300

    refactor: move swagger config file to config folder and change apis path

---
+++ src/config/swagger.config.js
@@ -0,0 +1,24 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+import __dirname from "../utils.js";
+
+const options = {
+  definition: {
+    openapi: "3.0.1",
+    info: { title: "Gaming Components", version: "1.0.0", description: "Gaming Components API"},
+  },
+  apis: [`${__dirname}/docs/**/*.yaml`],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const swaggerDocs = (app, port) => {
+  console.log(__dirname);
+  app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+  app.get("/api/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+
+  console.log(`Version 1 Docs are available at http://localhost:${port}/api/docs`);
+};
