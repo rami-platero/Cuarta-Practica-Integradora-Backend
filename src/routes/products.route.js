a03157083e56a175f39c0ba1ba710cb22f3e8f05
rami-platero <ramiplatero11@gmail.com>
2024-07-04 17:34:39 -0300

    feat: add upload middleware for product thumbnails

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -3,13 +3,14 @@ import { validateCreateProduct, validateDeleteProduct, validateGetProductById, v
 import { createProduct, deleteProduct, getMockingProducts, getProductById, getProducts, updateProduct } from "../controllers/product.controller.js";
 import { isAuthorized } from "../middlewares/authJwt.js";
 import { passportCall } from "../middlewares/passport.js";
+import { upload } from "../middlewares/upload.middleware.js";
 
 const router = Router();
 
 router.get("/", validateGetProducts, getProducts);
 router.get("/mockingproducts", getMockingProducts)
 router.get("/:pid", validateGetProductById, getProductById);
-router.post("/", validateCreateProduct, passportCall("jwt"), isAuthorized(["admin", "premium"]), createProduct);
+router.post("/", upload.array("thumbnails"), validateCreateProduct, passportCall("jwt"), isAuthorized(["admin", "premium"]), createProduct);
 router.put("/:pid", validateUpdateProduct, passportCall("jwt"), isAuthorized(["admin", "premium"]),updateProduct);
 router.delete("/:pid", validateDeleteProduct, passportCall("jwt"), isAuthorized(["admin", "premium"]),deleteProduct);
 
