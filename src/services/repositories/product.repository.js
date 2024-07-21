092e52aed2818a9810fec69da7f4a32854eb2547
rami-platero <ramiplatero11@gmail.com>
2024-07-21 19:03:39 -0300

    feat: send an email to the product owner when deleting a product

--- src/services/repositories/product.repository.js
+++ src/services/repositories/product.repository.js
@@ -1,6 +1,8 @@
 import { AppError } from "../../helpers/AppError.js";
 import { generateProduct } from "../../utils/faker.js";
+import { sendMail } from "../email/mailing.service.js";
 import { EErrors } from "../errors/enums.js";
+import { userService } from "../service.js";
 
 export default class ProductRepository {
   constructor(dao) {
@@ -17,7 +19,7 @@ export default class ProductRepository {
       status: body.status,
       stock: body.stock,
       thumbnails: thumbnails,
-      owner: body.owner
+      owner: body.owner,
     });
   };
 
@@ -45,6 +47,36 @@ export default class ProductRepository {
         message: "Error while trying to delete product.",
         code: EErrors.UNAUTHORIZED,
         cause: `You are not authorized to delete this product.`,
+      });
+    }
+    const foundOwner = await userService.getUser({ email: owner });
+    if (foundOwner && foundOwner.role === "premium") {
+      await sendMail({
+        targetUser: foundOwner.email,
+        subject: "Your product has been removed.",
+        html: `
+<body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 5px; background-color: #f9f9f9;">
+    <div style="text-align: center; padding: 10px 0;">
+        <h1 style="margin: 0; color: #d9534f;">Product Removed</h1>
+    </div>
+    <div style="margin-top: 20px;">
+        <p>Dear User,</p>
+        <p>We regret to inform you that one of your products has been removed from our system. Below are the details of the removed product:</p>
+        <div style="margin-top: 20px; padding: 10px; border: 1px solid #ddd; background-color: #fff; border-radius: 5px;">
+            <p style="margin: 5px 0;"><strong>Title:</strong> ${foundProduct.title}</p>
+            <p style="margin: 5px 0;"><strong>Description:</strong> ${foundProduct.description}</p>
+            <p style="margin: 5px 0;"><strong>Code:</strong> ${foundProduct.code}</p>
+            <p style="margin: 5px 0;"><strong>Stock:</strong> ${foundProduct.stock}</p>
+            <p style="margin: 5px 0;"><strong>Price:</strong> $${foundProduct.price}</p>
+            <p style="margin: 5px 0;"><strong>Category:</strong> ${foundProduct.category}</p>
+        </div>
+        <p>If you have any questions or need further assistance, please feel free to contact us.</p>
+    </div>
+    <div style="text-align: center; margin-top: 20px; color: #777;">
+        <p>Thank you for your understanding.</p>
+    </div>
+</body>
+`,
       });
     }
     return await this.dao.deleteOneById(id);
