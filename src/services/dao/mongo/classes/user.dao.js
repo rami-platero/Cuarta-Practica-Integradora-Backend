62b27c5d4fd543e24b5d01be1b766dd79bd96743
rami-platero <ramiplatero11@gmail.com>
2024-07-21 19:05:59 -0300

    feat: create a method to find all users and to delete users by a condition in the last_connection field

--- src/services/dao/mongo/classes/user.dao.js
+++ src/services/dao/mongo/classes/user.dao.js
@@ -1,13 +1,19 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 
 export default class UserMongo {
-    create = async (data) => {
-        return await User.create(data)
-    }
-    findOne = async (data) => {
-        return await User.findOne(data, {}, {lean: true})
-    }
-    updateOne = async (data, upFields) => {
-        return await User.updateOne(data, {$set: {...upFields}})
-    }
+  create = async (data) => {
+    return await User.create(data);
+  };
+  findOne = async (data) => {
+    return await User.findOne(data, {}, { lean: true });
+  };
+  updateOne = async (data, upFields) => {
+    return await User.updateOne(data, { $set: { ...upFields } });
+  };
+  findAll = async () => {
+    return await User.find().lean(true);
+  };
+  deleteManyWithLastConnectionBefore = async (before) => {
+    return await User.deleteMany({ last_connection: { $lt: before } });
+  };
 }
